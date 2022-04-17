5ba113e9608726d93cfe94ddec7d487364925ea6
Robin Hong <robinh.jsx@gmail.com>
2022-04-17 02:10:49 -0400

    rename variable

--- packages/rui-theme/src/ThemeProvider/functions/getTransition.ts
+++ packages/rui-theme/src/ThemeProvider/functions/getTransition.ts
@@ -2,14 +2,14 @@ import type { BaseTheme } from '../../types'
 
 export interface TransitionOptions {
 	properties?: React.CSSProperties['transitionProperty'][]
-	timeout?: string | number
+	duration?: string | number
 }
 
 const parseDuration = (value?: number | string) => (typeof value === 'number' ? `${value}ms` : value)
 
 export const getTransition = (theme: BaseTheme) => (options?: TransitionOptions) => {
-	const { properties = ['all'], timeout } = options || {}
+	const { properties = ['all'], duration } = options || {}
 	return properties
-		.map(property => `${property} ${parseDuration(timeout || theme.transition.duration)} ${theme.transition.ease}`)
+		.map(property => `${property} ${parseDuration(duration || theme.transition.duration)} ${theme.transition.ease}`)
 		.join(', ')
 }
