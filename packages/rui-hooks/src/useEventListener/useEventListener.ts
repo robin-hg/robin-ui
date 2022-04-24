c6c2b7b3cdd0349d56dfac7699e1929536dcf231
Robin Hong <robinh.jsx@gmail.com>
2022-04-23 21:20:43 -0400

    rename variables

--- packages/rui-hooks/src/useEventListener/useEventListener.ts
+++ packages/rui-hooks/src/useEventListener/useEventListener.ts
@@ -2,10 +2,10 @@ import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect'
 import { useMutableCallback } from '../useMutableCallback'
 
 type IUseEventListener = {
-	<K extends keyof WindowEventMap>(eventName: K, listener: (event: WindowEventMap[K]) => void): void
+	<K extends keyof WindowEventMap>(eventName: K, handler: (event: WindowEventMap[K]) => void): void
 	<K extends keyof HTMLElementEventMap, T extends HTMLElement>(
 		eventName: K,
-		listener: (event: HTMLElementEventMap[K]) => void,
+		handler: (event: HTMLElementEventMap[K]) => void,
 		element: T
 	): void
 }
@@ -16,16 +16,16 @@ export const useEventListener: IUseEventListener = <
 	T extends HTMLElement
 >(
 	eventName: KW | KH,
-	listener: (event: WindowEventMap[KW] | HTMLElementEventMap[KH] | Event) => void,
+	handler: (event: WindowEventMap[KW] | HTMLElementEventMap[KH] | Event) => void,
 	element?: T | null
 ) => {
 	const el = element || window
-	const savedListener = useMutableCallback(listener)
+	const savedHandler = useMutableCallback(handler)
 
 	useIsomorphicLayoutEffect(() => {
-		el.addEventListener(eventName, savedListener, { capture: true })
+		el.addEventListener(eventName, savedHandler, { capture: true })
 		return () => {
-			el.removeEventListener(eventName, savedListener, { capture: true })
+			el.removeEventListener(eventName, savedHandler, { capture: true })
 		}
 	}, [])
 }
