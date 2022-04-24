3c5dd9d459aa11df805628e1ebcd8febea792f8d
Robin Hong <robinh.jsx@gmail.com>
2022-04-24 16:14:06 -0400

    add Pagination

---
+++ packages/rui-core/src/components/Pagination/Pagination.style.ts
@@ -0,0 +1,43 @@
+import styled from '@rui/styles'
+
+import { Button } from '../Button'
+
+interface PaginationContainerProps {
+	$align: 'left' | 'right' | 'center'
+}
+
+export const PaginationContainer = styled.div<PaginationContainerProps>(
+	({ theme }) => ({
+		display: 'flex',
+		alignItems: 'center',
+		gap: theme.spacing.sm
+	}),
+	({ $align }) =>
+		({
+			left: {
+				justifyContent: 'flex-start'
+			},
+			right: {
+				justifyContent: 'flex-end'
+			},
+			center: {
+				justifyContent: 'center'
+			}
+		}[$align])
+)
+
+export const PageButton = styled(Button)({
+	minWidth: '3.2rem',
+	height: '3.2rem',
+	padding: 0,
+	fontWeight: 'normal'
+})
+
+export const Elipsis = styled.div(({ theme }) => ({
+	width: '3.2rem',
+	color: theme.fn.getColor('surface.onBase'),
+	textAlign: 'center',
+	'&::before': {
+		content: '"..."'
+	}
+}))
---
+++ packages/rui-core/src/components/Pagination/Pagination.tsx
@@ -0,0 +1,117 @@
+import type { DefaultProps } from '@rui/types'
+import React from 'react'
+
+import { PaginationContainer, PageButton, Elipsis } from './Pagination.style'
+import { ChevronLeft, ChevronRight } from '@rui/icons'
+
+export interface Props extends DefaultProps<HTMLDivElement, 'onChange'> {
+	totalItems: number
+	itemsPerPage: number
+	currentPage: number
+	/**
+	 * Max number of page buttons. Min 5.
+	 * @default 7
+	 */
+	maxLength?: number
+	/**
+	 * Alignment.
+	 * @default right
+	 */
+	align?: 'left' | 'right' | 'center'
+	/**
+	 * Button color.
+	 * @default primary
+	 */
+	color?: string
+	onChange?: (page: number) => void
+}
+
+export const Pagination = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
+	const {
+		totalItems,
+		itemsPerPage,
+		currentPage,
+		maxLength = 7,
+		align = 'right',
+		color = 'primary',
+		onChange,
+		...otherProps
+	} = props
+
+	const numberOfPages = Math.ceil(totalItems / itemsPerPage)
+	const numSiblings = Math.max(5, maxLength) / 2
+	const currentIndex = currentPage - 1
+	const minIndex = currentIndex - Math.floor(numSiblings)
+	const maxIndex = currentIndex + Math.ceil(numSiblings)
+
+	const visiblePages = Array(numberOfPages)
+		.fill(null)
+		.map((_, i) => i + 1)
+		.slice(
+			Math.max(0, minIndex + Math.min(0, numberOfPages - maxIndex)),
+			Math.min(numberOfPages, maxIndex - Math.min(0, minIndex))
+		)
+
+	const navigate = (page: number) => {
+		if (page < 1 || page > numberOfPages) {
+			return
+		}
+		onChange?.(page)
+	}
+
+	return (
+		<PaginationContainer ref={ref} $align={align} {...otherProps}>
+			<PageButton
+				variant="text"
+				size="sm"
+				color="surface.onBase"
+				disabled={currentPage === 1}
+				onClick={() => navigate(currentPage - 1)}
+				startAdornment={<ChevronLeft />}
+				aria-label={`Go to previous page`}
+			/>
+			{visiblePages.map((page, i) => {
+				switch (i) {
+					case 0:
+						page = 1
+						break
+					case 1:
+						if (page > 2) {
+							return <Elipsis key={page} />
+						}
+						break
+					case visiblePages.length - 1:
+						page = numberOfPages
+						break
+					case visiblePages.length - 2:
+						if (page < numberOfPages - 1) {
+							return <Elipsis key={page} />
+						}
+						break
+				}
+				return (
+					<PageButton
+						key={page}
+						variant={page === currentPage ? 'filled' : 'text'}
+						size="sm"
+						color={page === currentPage ? color : 'surface.onBase'}
+						onClick={() => navigate(page)}
+						aria-label={`Go to page ${page}`}>
+						{page}
+					</PageButton>
+				)
+			})}
+			<PageButton
+				variant="text"
+				size="sm"
+				color="surface.onBase"
+				disabled={currentPage === numberOfPages}
+				onClick={() => navigate(currentPage + 1)}
+				endAdornment={<ChevronRight />}
+				aria-label={`Go to next page`}
+			/>
+		</PaginationContainer>
+	)
+})
+
+Pagination.displayName = 'Pagination'
