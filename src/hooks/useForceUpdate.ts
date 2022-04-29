3154301d14f2f4d038acb50f9e3ffd6455c02526
Robin Hong <robinh.jsx@gmail.com>
2022-04-29 13:13:21 -0400

    remove old files

--- src/hooks/useForceUpdate.ts
+++
@@ -1,8 +0,0 @@
-import { useCallback, useState } from 'react'
-
-const useForceUpdate = () => {
-	const [, update] = useState({})
-	return useCallback(() => update({}), [])
-}
-
-export default useForceUpdate
--- src/style/styled/index.ts
+++
@@ -1,1 +0,0 @@
-export { default, Styled } from './styled'
