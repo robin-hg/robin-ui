72934d5cb9b9edab51edfb16d3dc8e09d74c0efd
Robin Hong <robinh.jsx@gmail.com>
2022-05-16 02:49:48 -0400

    allow min, max to be optional

--- packages/rui-utils/src/clampNumber/clampNumber.ts
+++ packages/rui-utils/src/clampNumber/clampNumber.ts
@@ -1,3 +1,10 @@
-export const clampNumber = (value: number, min: number, max: number) => {
-	return Math.min(Math.max(value, min), max)
+export const clampNumber = (value: number, min?: number, max?: number) => {
+	let number = value
+	if (min && min > number) {
+		number = min
+	}
+	if (max && max < number) {
+		number = max
+	}
+	return number
 }
