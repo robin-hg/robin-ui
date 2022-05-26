d3e9fed2c1d66c0a52e3e6341128ccaf5aecbdfb
Robin Hong <robinh.jsx@gmail.com>
2022-05-26 02:01:17 -0400

    add Image component

---
+++ packages/rui-core/src/components/Image/Image.stories.tsx
@@ -0,0 +1,18 @@
+import type { Story, Meta } from '@storybook/react'
+
+import { Image, type Props } from './Image'
+
+export default {
+	title: 'Display/Image',
+	component: Image,
+	args: {
+		src: 'https://images.ctfassets.net/hrltx12pl8hq/5qusjAtcAaetdPsing4jR6/2db2f75abd2840caa09d21312b4fc6c8/animals-wildlife-shutterstock_1066200380.jpg?fit=fill&w=1024&h=683&fm=webp',
+		alt: 'Tiger',
+		radius: 'md'
+	},
+	argTypes: {
+		radius: { control: { type: 'radio' }, options: ['xs', 'sm', 'md', 'lg', 'xl'] }
+	}
+} as Meta<Props>
+
+export const Default: Story<Props> = args => <Image {...args} />
---
+++ packages/rui-core/src/components/Image/Image.style.ts
@@ -0,0 +1,34 @@
+import type { SizeValue } from '@rui/theme'
+import styled from '@rui/styles'
+import { Fade } from '../Transition'
+
+interface ImageContainerProps {
+	$radius: SizeValue
+	$width: string | number
+	$height: string | number
+	$fit: React.CSSProperties['objectFit']
+}
+
+export const ImageContainer = styled.div<ImageContainerProps>(({ theme, $radius, $width, $height, $fit }) => ({
+	position: 'relative',
+	minHeight: theme.size.xl,
+	'& > img': {
+		display: 'block',
+		objectFit: $fit,
+		height: $height,
+		width: $width,
+		borderRadius: theme.fn.getSize($radius, theme.radius)
+	}
+}))
+
+export const Placeholder = styled(Fade)(({ theme }) => ({
+	position: 'absolute',
+	top: 0,
+	left: 0,
+	width: '100%',
+	height: '100%',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	background: theme.palette.surface.variant
+}))
