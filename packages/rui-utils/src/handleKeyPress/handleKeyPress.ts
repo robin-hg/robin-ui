ab5abfa9a3484cf9ec303b0e3af344eb42cc8978
Robin Hong <robinh.jsx@gmail.com>
2022-05-31 22:32:37 -0400

    remove unused util

--- packages/rui-utils/src/handleKeyPress/handleKeyPress.ts
+++
@@ -1,9 +0,0 @@
-export const handleKeyPress =
-	<T extends HTMLElement>(key: string, handler: (event: React.KeyboardEvent<T>) => void) =>
-	(event: React.KeyboardEvent<T>) => {
-		if (event.key === key) {
-			event.stopPropagation()
-			event.preventDefault()
-			handler(event)
-		}
-	}
