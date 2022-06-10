fbf694d139f83e4a076378f826db58af8fd6926e
Robin Hong <robinh.jsx@gmail.com>
2022-06-10 05:57:57 -0400

    fix build

--- packages/styles/src/index.ts
+++ packages/styles/src/index.ts
@@ -1,4 +1,9 @@
+import type { RobinTheme } from '@robin-ui/theme'
 export { css, keyframes, jsx, Global, useTheme } from '@emotion/react'
 export { default, sxc } from './styled'
+
+declare module '@emotion/react' {
+	export interface Theme extends RobinTheme {}
+}
 
 export * from './types'
