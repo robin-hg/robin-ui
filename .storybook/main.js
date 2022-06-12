b7c10e9aefe53cfb6a49e36a52043e5b65ee32b6
Robin Hong <robinh.jsx@gmail.com>
2022-06-12 08:09:59 -0400

    re-enable storyStoreV7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,14 @@
 module.exports = {
 	core: { builder: '@storybook/builder-vite', disableTelemetry: true },
-	stories: ['../packages/**/*.stories.@(tsx|mdx)'],
-	addons: ['@storybook/addon-controls', '@storybook/addon-actions', '@storybook/addon-a11y', 'storybook-dark-mode'],
+	stories: ['../packages/core/src/**/*.stories.@(tsx|mdx)'],
+	addons: [
+		'@storybook/addon-controls',
+		'@storybook/addon-actions',
+		'@storybook/addon-a11y',
+		'storybook-dark-mode'
+	],
 	framework: '@storybook/react',
-	features: { emotionAlias: false },
+	features: { emotionAlias: false, storyStoreV7: true },
 	viteFinal: config => {
 		const path = require('path')
 		// workaround for hoisting issues with pnpm
