36a490ae6c6177d92a8f89c7d7a94f4194b1b29d
Robin Hong <robinh.jsx@gmail.com>
2022-06-16 05:34:18 -0400

    setup test utils

---
+++ packages/test-utils/src/testUtils.tsx
@@ -0,0 +1,22 @@
+import {
+	render,
+	renderHook,
+	type RenderOptions,
+	type RenderHookOptions
+} from '@testing-library/react'
+import { RobinProvider } from '@robin-ui/core'
+
+const Wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+	<RobinProvider>{children}</RobinProvider>
+)
+
+const customRender = (ui: React.ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
+	render(ui, { wrapper: Wrapper, ...options })
+
+const customRenderHook = <Result, Props>(
+	hook: (initialProps: Props) => Result,
+	options?: Omit<RenderHookOptions<Props>, 'wrapper'>
+) => renderHook<Result, Props>(hook, { wrapper: Wrapper, ...options })
+
+export * from '@testing-library/react'
+export { customRender as render, customRenderHook as renderHook }
