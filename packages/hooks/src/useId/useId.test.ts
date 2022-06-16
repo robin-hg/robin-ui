2cebed13413ad3c3a8936d8640b5b65b618ad025
Robin Hong <robinh.jsx@gmail.com>
2022-06-16 07:13:56 -0400

    add a hook test

---
+++ packages/hooks/src/useId/useId.test.ts
@@ -0,0 +1,18 @@
+import { renderHook } from '@robin-ui/test-utils'
+
+import { useId } from './useId'
+
+describe('useId', () => {
+	it('should generate unique ids', () => {
+		const { result: result1 } = renderHook(useId)
+		const { result: result2 } = renderHook(useId)
+
+		expect(result1.current).not.toBe(result2.current)
+	})
+
+	it('should return given id', () => {
+		const { result } = renderHook(useId, { initialProps: 'fixed-id' })
+
+		expect(result.current).toBe('fixed-id')
+	})
+})
