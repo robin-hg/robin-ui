9361bca310ac85b6f4c6af9bfacc36ceec38b962
Robin Hong <robinh.jsx@gmail.com>
2022-08-03 02:55:52 -0400

    fix vite optimize deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const { mergeConfig } = require('vite')
 
 module.exports = {
   core: { builder: '@storybook/builder-vite', disableTelemetry: true },
-  stories: ['../packages/core/src/**/*.stories.@(tsx|mdx)'],
+  stories: ['../packages/core/src/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-controls',
     '@storybook/addon-actions',
@@ -14,8 +14,26 @@ module.exports = {
   viteFinal: config => {
     const path = require('path')
     // workaround for hoisting issues with pnpm
+    console.log(
+      `${path.relative(config.root, path.resolve(__dirname, '../packages'))}/**/*.stories.tsx`
+    )
     return mergeConfig(config, {
-      root: path.dirname(require.resolve('@storybook/builder-vite'))
+      root: path.dirname(require.resolve('@storybook/builder-vite')),
+      optimizeDeps: {
+        include: [
+          ...(config.optimizeDeps?.include ?? []),
+          '@storybook/theming',
+          'storybook-dark-mode',
+          '@emotion/react',
+          'framer-motion',
+          '@floating-ui/react-dom',
+          'react-number-format',
+          'colord',
+          'colord/plugins/a11y',
+          'colord/plugins/lch',
+          'colord/plugins/mix'
+        ]
+      }
     })
   }
 }
