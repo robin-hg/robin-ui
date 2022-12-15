6d190c34c8480fd46c46bf789e0c793e6924087f
Robin Hong <robinh.jsx@gmail.com>
2022-12-15 09:29:50 -0500

    remove workaround

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,4 @@
 import type { StorybookConfig } from '@storybook/react-vite'
-import { mergeConfig } from 'vite'
-import path from 'path'
 
 const storybokConfig: StorybookConfig = {
   stories: ['../packages/core/src/**/*.stories.tsx'],
@@ -8,12 +6,7 @@ const storybokConfig: StorybookConfig = {
   framework: {
     name: '@storybook/react-vite',
     options: { fastRefresh: true }
-  },
-  // workaround for hoisting issues with pnpm
-  viteFinal: config =>
-    mergeConfig(config, {
-      root: path.dirname(require.resolve('@storybook/builder-vite'))
-    })
+  }
 }
 
 export default storybokConfig
