3cf3ba7157b6bc50642f108b2fbca34625944f7d
Robin Hong <robinh.jsx@gmail.com>
2022-12-29 15:30:06 -0500

    fix transpile

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  transpilePackages: ['@robin-ui/*']
 }
 
 module.exports = nextConfig
