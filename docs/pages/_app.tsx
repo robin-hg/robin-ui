ba682835ee484536fac85b24c7b445f3f60d608d
Robin Hong <robinh.jsx@gmail.com>
2022-12-29 20:28:56 -0500

    remove next default page

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -2,8 +2,6 @@ import type { AppProps } from 'next/app'
 
 import { RobinProvider } from '@robin-ui/core'
 
-import '../styles/globals.css'
-
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <RobinProvider>
