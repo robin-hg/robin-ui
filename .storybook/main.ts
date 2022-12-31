944abc18b6d7e55af7e77581b0db965ff5d29f58
Robin Hong <robinh.jsx@gmail.com>
2022-12-30 23:49:11 -0500

    add storysource

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,12 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 const storybokConfig: StorybookConfig = {
   stories: ['../packages/core/src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-controls', '@storybook/addon-a11y', 'storybook-dark-mode'],
+  addons: [
+    '@storybook/addon-controls',
+    '@storybook/addon-a11y',
+    '@storybook/addon-storysource',
+    'storybook-dark-mode'
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: { fastRefresh: true }
