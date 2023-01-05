9cc43fc3578a149ce90eccbb9f40ae6944052668
Robin Hong <robinh.jsx@gmail.com>
2023-01-05 01:09:59 -0500

    update test

--- packages/utils/src/color/color.test.ts
+++ packages/utils/src/color/color.test.ts
@@ -4,11 +4,11 @@ describe('color', () => {
   it('should generate color shades from a base color', () => {
     const shades = generateShades('#ff0000')
     expect(shades).toEqual([
-      '#ffd8bd',
-      '#ffad88',
-      '#ff7e55',
-      '#ff4121',
-      '#ff0000',
+      '#ffe5d6',
+      '#ffbca1',
+      '#ff916e',
+      '#ff613d',
+      '#ff0902',
       '#e00000',
       '#ad0000',
       '#7d0000',
@@ -19,27 +19,27 @@ describe('color', () => {
 
   it('should generate 5 shades', () => {
     const shades = generateShades('#ff0000', 5)
-    expect(shades).toEqual(['#ffe1c5', '#ff875d', '#ff0000', '#b60000', '#581000'])
+    expect(shades).toEqual(['#ffcaaf', '#ff7049', '#dd3219', '#9e0000', '#440000'])
   })
 
   it('should generate 15 shades', () => {
     const shades = generateShades('#ff0000', 15)
     expect(shades).toEqual([
-      '#ffd2b7',
-      '#ffb593',
-      '#ff9670',
-      '#ff7850',
-      '#ff512e',
-      '#ff0000',
-      '#ff0000',
-      '#ff0000',
-      '#c60000',
-      '#a70000',
-      '#860000',
-      '#660000',
-      '#4b0100',
-      '#300200',
-      '#1b0000'
+      '#ffefe5',
+      '#ffd3c0',
+      '#ffb79c',
+      '#ff9c7c',
+      '#ff7d5a',
+      '#ff5b38',
+      '#ff3116',
+      '#ea0401',
+      '#d30000',
+      '#b30000',
+      '#920000',
+      '#710600',
+      '#550d00',
+      '#380c00',
+      '#200200'
     ])
   })
 })
